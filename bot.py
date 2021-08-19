2c5cca80e41da7616ab3155b47d772aa8e351b15
alexeyev <anton.m.alexeyev@gmail.com>
2021-08-19 15:21:44 +0300

    Bot init

---
+++ bot.py
@@ -0,0 +1,56 @@
+# coding: utf-8
+
+import configparser
+import logging
+import camera
+import telebot
+
+logger = logging.getLogger("counting-crows")
+logger.setLevel(logging.DEBUG)
+
+fh = logging.FileHandler("everything.log")
+fh.setLevel(logging.DEBUG)
+logger.addHandler(fh)
+
+parser = configparser.ConfigParser()
+parser.read("configs/bot_config.ini")
+ACCESS_KEY = parser["telegram"]["key"]
+
+bot = telebot.TeleBot(ACCESS_KEY, num_threads=4)
+content_types = "audio, sticker, video, video_note, location, contact, new_chat_members, " \
+                "left_chat_member, new_chat_title, new_chat_photo, delete_chat_photo, group_chat_created, " \
+                "supergroup_chat_created, channel_chat_created, migrate_to_chat_id, migrate_from_chat_id, " \
+                "pinned_message, document, photo".split(", ")
+
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+    logger.debug(message)
+    bot.reply_to(message, "Привет, это бот, который учится считать ворон. Используйте команду /bang.")
+
+
+@bot.message_handler(commands=['bang'])
+def send_welcome(message):
+    logger.debug(message)
+    username = message.from_user.username
+    date = message.date
+    prefix = "data/%d_%d_%s" % (date, message.message_id, username)
+    image = camera.shoot(prefix)
+    bot.send_photo(message.chat.id, image)
+
+
+
+
+@bot.message_handler(content_types=['voice'])
+def handle_voice(message):
+    logger.debug(message)
+    bot.reply_to(message, "Не очень удобно сейчас слушать голосовые, давайте лучше командами.")
+
+
+@bot.message_handler(content_types=["text"] + content_types)
+def handle_text(message):
+    logger.debug(message)
+    bot.reply_to(message, "Увы, бот воспринимает только команды, начинающиеся на \"/\".")
+
+
+bot.polling()
---
+++ camera.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+from time import sleep
+
+from picamera import PiCamera
+
+camera = PiCamera()
+
+
+def shoot(prefix: str):
+    camera.start_preview()
+    sleep(3)
+    camera.capture(prefix + ".jpg")
+    camera.stop_preview()
+    return open('out.jpg', 'rb')
