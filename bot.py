1a3ce141970502c2a94d65917e1ebd14f8ae521f
alexeyev <anton.m.alexeyev@gmail.com>
2021-08-24 19:35:49 +0300

    Saving for CRON-based routines

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ import logging
 import camera
 import telebot
 
-logger = logging.getLogger("counting-crows")
+logger = logging.getLogger("counting-crops")
 logger.setLevel(logging.DEBUG)
 
 fh = logging.FileHandler("everything.log")
@@ -20,13 +20,13 @@ bot = telebot.TeleBot(ACCESS_KEY, num_threads=4)
 content_types = "audio, sticker, video, video_note, location, contact, new_chat_members, " \
                 "left_chat_member, new_chat_title, new_chat_photo, delete_chat_photo, group_chat_created, " \
                 "supergroup_chat_created, channel_chat_created, migrate_to_chat_id, migrate_from_chat_id, " \
-                "pinned_message, document, photo".split(", ")
+                "pinned_message, document, photo, text".split(", ")
 
 
 @bot.message_handler(commands=["start", "help"])
 def send_welcome(message):
     logger.debug(message)
-    bot.reply_to(message, "Привет, это бот, который учится считать ворон. Ещё в процессе разработки. "
+    bot.reply_to(message, "Привет, это бот, который докладывает обстановку. Ещё в процессе разработки. "
                           "Используйте команду /bang.")
 
 
@@ -37,19 +37,13 @@ def process_bang(message):
     date = message.date
     prefix = "data/%d_%d_%s" % (date, message.message_id, username)
     image = camera.shoot(prefix)
-    bot.send_photo(message.chat.id, image, caption="Сколько ворон на фотке?")
+    bot.send_photo(message.chat.id, image, caption="""¯\\_(ツ)_/¯""")
 
 
 @bot.message_handler(content_types=["voice"])
 def handle_voice(message):
     logger.debug(message)
     bot.reply_to(message, "Не очень удобно сейчас слушать голосовые, давайте лучше командами.")
-
-
-@bot.message_handler(content_types=["text"])
-def handle_other(message):
-    logger.debug(message)
-    bot.reply_to(message, "Спасибо, залогировал (пока сохранение чисел и валидация не работают).")
 
 
 @bot.message_handler(content_types=content_types)
