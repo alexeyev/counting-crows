e4bc1c4a06effe89ff8c7061d3a8d1628b0c16fd
alexeyev <anton.m.alexeyev@gmail.com>
2021-08-24 19:31:56 +0300

    Saving for CRON-based routines

--- camera.py
+++ camera.py
@@ -12,3 +12,16 @@ def shoot(prefix: str, timeout_for_focus_seconds=5):
     camera.capture(prefix + ".jpg")
     camera.stop_preview()
     return open(prefix + ".jpg", "rb")
+
+
+if __name__ == "__main__":
+
+    import datetime
+
+    date_part = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    prefix = "data/pot_%d" % date_part
+
+    try:
+        image = shoot(prefix, timeout_for_focus_seconds=7)
+    except Exception as e:
+        print("UnSuccessful")
